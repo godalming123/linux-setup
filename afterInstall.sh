2d25ae0c6a196a7428bab7f6965532f17bf73f14
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-12-10 17:31:08 +0000

    test new features

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,11 +1,77 @@
 #!/usr/bin/env bash
 clear
-read -p "pacadge manager: " packadgeManager
 
+declare -A codeToInstallThemes
+codeToInstallThemes[Zorin]=`git clone https://github.com/ZorinOS/zorin-desktop-themes.git
+sudo cp -r zorin-desktop-themes/Z\* /usr/share/themes`
+
+select themeToInstall in "${!codeToINstallThemes[@]}"; do
+    codeToInstallThemes["$themeToInstall"]
+done
+
+
+#=========basic functions=========
 runIfBool(){
     read -p "$1 y/N: " response
     if [ $response == "y" ]; then $2; fi
 }
+
+bannerMessage(){
+    sideDashes=$(($($(tput cols) - ${#$1}) / 2))
+    
+    tput cup 0 $sideDashes
+    tput bold
+    tput smul
+    setaf 2
+    
+    echo $1
+    
+    tput sgr0
+    tput cup $( tput lines ) 0
+}
+# =========test bannerMessage function=========
+# tput cup $sideDashes
+# tput bold
+# tput setaf 2
+# echo "hello"
+# bannerMessage "test"
+
+# =========get packadge manager=========
+select packadgeManager in apt-get pacman dnf custom unsopartedPackadge; do
+    customPackadgeManger(){
+        read -p "Command to install packadge (no need to add sudo): " $installPackCommand
+        read -p "Command to update system (no need to add sudo): " $updateSystCommand
+    }
+    
+    case $packadgeManager in
+        apt-get)
+            installPackCommand="apt-get install"
+            updateSystCommand="apt-get upgrade"
+            break
+        ;;
+        pacman)
+            installPackCommand="pacman -S"
+            updateSystCommand="pacman -U"
+            break
+        ;;
+        dnf)
+            installPackCommand="dnf install"
+            updateSystCommand="dnf upgrade"
+            break
+        ;;
+        custom)
+            customPackadgeManger
+            break
+        ;;
+        *)
+            echo "This packadge manager does not have specified commands to install packadges and update the sytem so you must enter the cammands manually."
+            customPackadgeManger
+            break
+        ;;
+    esac
+done
+
+read -p "pacadge manager: " packadgeManager
 
 installApp(){
     runIfBool "install $1?" "sudo $packadgeManager install $1"
@@ -14,6 +80,7 @@ installApp(){
 update(){
     sudo $packadgeManager update
 }
+
 
 #apps
 echo "==============================UPDATING SYSTEM=============================="
@@ -24,6 +91,7 @@ installApp gnome-tweak-tool
 installApp tilda
 installApp python3-pip
 sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
 installApp codium
 
 echo "====================INSTALLING GNOME SHELL EXTENSIONS======================"
@@ -35,14 +103,3 @@ runIfBool "install flutter?" '
   installApp "clang cmake ninja-build pkg-config libgtk-3-dev"
 '
 
-#thing
-bitarray>codeToInstallThemes = (
-    ["Zorin"] = '
-        git clone https://github.com/ZorinOS/zorin-desktop-themes.git
-        sudo cp -r zorin-desktop-themes/Z\* /usr/share/themes
-    '
-]
-bitarray>select themeToInstall in !codeToInstallThemes[*]
-do
-    codeToInstallThemes["$themeToInstall"]
-done
